9a72153ee3d962006060e3d32153ce0426c7cd25
Samjay <barnabassampana89@gmail.com>
2024-02-29 12:08:36 +0000

    added to single course

--- controller/user/courses.js
+++ controller/user/courses.js
@@ -41,7 +41,7 @@ router.get('/single/:id', (req, res) => {
             { $match: { _id: new mongoose.Types.ObjectId(id), status: 'ACTIVE'} },
             {
                 $project: {
-                    _id: 1, title: 1, status: 1, course: 1, createdAt: 1,
+                    _id: 1, title: 1, description:1, thumbnail:1, link:1,price:1,category:1,archived:1, status: 1, course: 1, createdAt: 1,
                     "lessons": {
                         "$filter": {
                             "input": "$lessons",
